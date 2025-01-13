dbd3ae2193db39004e22c216e5909aab206e4b8a
ByeongGyu Jeon <cube9928@naver.com>
2025-01-13 12:42:32 +0900

    16강

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,6 +1,25 @@
+type PostDto = {
+  id: number;
+  createDate: String;
+  modifyDate: String;
+  authorId: number;
+  authorName: string;
+  title: string;
+  published: boolean;
+  listed: boolean;
+};
+
+type PostItemPageDto = {
+  currentPageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+  items: PostDto[];
+};
+
 export default async function page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
-  const body = await response.json();
+  const body: PostItemPageDto = await response.json();
 
   return (
     <div>
@@ -9,7 +28,7 @@ export default async function page() {
       <div>totalPages : {body.totalPages}</div>
       <div>totalItems : {body.totalItems}</div>
       <ul>
-        {body.items.map((item: any) => (
+        {body.items.map((item: PostDto) => (
           <li key={item.id} className="border-[2px] border-[red] my-3">
             <div>id : {item.id}</div>
             <div>createDate : {item.createDate}</div>
