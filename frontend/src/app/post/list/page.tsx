5aa6c570df863a6b35c51f2ae6ee34a7d775767f
ByeongGyu Jeon <cube9928@naver.com>
2025-01-13 14:58:34 +0900

    21강

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -3,30 +3,52 @@ import type { components } from "@/lib/backend/apiV1/schema";
 type PostDto = components["schemas"]["PostDto"];
 type PageDtoPostDto = components["schemas"]["PageDtoPostDto"];
 
-export default async function page() {
-  const response = await fetch("http://localhost:8080/api/v1/posts");
+export default async function page({
+  searchParams,
+}: {
+  searchParams: {
+    searchKeywordType: "title" | "content";
+    searchKeyword: string;
+  };
+}) {
+  const { searchKeyword = "", searchKeywordType = "title" } =
+    await searchParams;
+  const response = await fetch(
+    `http://localhost:8080/api/v1/posts?searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`
+  );
   const body: PageDtoPostDto = await response.json();
 
   return (
     <div>
-      <div>currentPageNumber : {body.currentPageNumber}</div>
-      <div>pageSize : {body.pageSize}</div>
-      <div>totalPages : {body.totalPages}</div>
-      <div>totalItems : {body.totalItems}</div>
-      <ul>
-        {body.items.map((item: PostDto) => (
-          <li key={item.id} className="border-[2px] border-[red] my-3">
-            <div>id : {item.id}</div>
-            <div>createDate : {item.createDate}</div>
-            <div>modifyDate : {item.modifyDate}</div>
-            <div>authorId : {item.authorId}</div>
-            <div>authorName : {item.authorName}</div>
-            <div>title : {item.title}</div>
-            <div>published : {`${item.published}`}</div>
-            <div>listed : {`${item.listed}`}</div>
-          </li>
-        ))}
-      </ul>
+      <form>
+        <select name="searchKeywordType" defaultValue={searchKeywordType}>
+          <option value="title">제목</option>
+          <option value="content">내용</option>
+        </select>
+        <input type="text" name="searchKeyword" defaultValue={searchKeyword} />
+        <button type="submit">검색</button>
+      </form>
+
+      <div>
+        <div>currentPageNumber : {body.currentPageNumber}</div>
+        <div>pageSize : {body.pageSize}</div>
+        <div>totalPages : {body.totalPages}</div>
+        <div>totalItems : {body.totalItems}</div>
+        <ul>
+          {body.items.map((item: PostDto) => (
+            <li key={item.id} className="border-[2px] border-[red] my-3">
+              <div>id : {item.id}</div>
+              <div>createDate : {item.createDate}</div>
+              <div>modifyDate : {item.modifyDate}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>title : {item.title}</div>
+              <div>published : {`${item.published}`}</div>
+              <div>listed : {`${item.listed}`}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
